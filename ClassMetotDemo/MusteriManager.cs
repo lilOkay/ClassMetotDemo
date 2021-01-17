82d5006650f8f0676ebc6a7c6d74d303d7be0070
lilOkay <gncokay@gmail.com>
2021-01-17 20:18:38 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri eklendi! : " + musteri.Adi + " " + musteri.Soyadi);
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri silindi! : " + musteri.Adi + " " + musteri.Soyadi);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "Okay";
+            musteri1.Soyadi = "Genc";
+            musteri1.Yasi = 16;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adi = "lionel";
+            musteri2.Soyadi = "messi";
+            musteri2.Yasi = 35;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+            foreach(var musteri in musteriler)
+            {
+                Console.WriteLine("---Müşteri-- -");
+                Console.WriteLine(musteri.Adi);
+                Console.WriteLine(musteri.Soyadi);
+                Console.WriteLine(musteri.Yasi);
+            }
+            MusteriManager musterimanager = new MusteriManager();
+            {
+                musterimanager.Ekle(musteri1);
+                Console.WriteLine("---------------------------------");
+                musterimanager.Sil(musteri2);
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
